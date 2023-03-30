5d0122c99bb42029adda2feeb1327f18858e53b3
Gonzalo Longe <gonlonge@icloud.com>
2023-03-30 10:44:22 +0200

    Update CartPage.jsx

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { useProductsStore } from "../components/AddToCart";
 import { CountDisplay } from "../components/styled-components/AddToCart.styles";
